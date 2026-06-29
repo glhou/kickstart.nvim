0b8fdbd3274ee0e62f02c5dbd58c6d20887e0bca
glouedec-bhj <glenn.louedec@bhj-soft.com>
2026-06-29 15:42:59 +0900

    Better relative number color

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -3,6 +3,15 @@ return {
   name = 'catppuccin',
   priority = 1000,
   config = function()
+    require('catppuccin').setup {
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.lavender, bold = true },
+          LineNrAbove = { fg = colors.overlay1 },
+          LineNrBelow = { fg = colors.overlay1 },
+        }
+      end,
+    }
     vim.cmd.colorscheme 'catppuccin-macchiato'
   end,
 }
