c0e949f7e4e3eb90b0de92a8ada3b007d71bfacd
glouedec-bhj <glenn.louedec@bhj-soft.com>
2026-07-21 13:29:09 +0900

    Remove some restriction from hardtime

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -2,8 +2,5 @@ return {
   'm4xshen/hardtime.nvim',
   lazy = false,
   dependencies = { 'MunifTanjim/nui.nvim' },
-  opts = {
-    max_count = 1,
-    allow_different_key = false,
-  },
+  opts = {},
 }
