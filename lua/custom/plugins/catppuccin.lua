1ff0049cd4ba7501a52e33fe04fbf3bb7c2a972d
glouedec-bhj <glenn.louedec@bhj-soft.com>
2026-09-18 15:45:17 +0900

    more colors and some change

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -6,7 +6,7 @@ require('catppuccin').setup {
       LineNrAbove = { fg = colors.mauve },
       LineNrBelow = { fg = colors.mauve }, -- someting
       ['@comment.todo.comment'] = {
-        fg = colors.peach,
+        fg = colors.mauve,
         bg = 'NONE',
       },
       ['@comment.warning.comment'] = {
@@ -17,7 +17,19 @@ require('catppuccin').setup {
         fg = colors.sky,
         bg = 'NONE',
       },
-      MiniHipatternsTodo = { fg = colors.peach, bg = 'NONE', bold = true }, -- TODO
+      ['@comment.error.comment'] = {
+        fg = colors.base,
+        bg = colors.red,
+      },
+      ['@comment.hint.comment'] = {
+        fg = colors.yellow,
+        bg = 'NONE',
+      },
+      ['@comment.ok.comment'] = {
+        fg = colors.green,
+        bg = 'NONE',
+      },
+      MiniHipatternsTodo = { fg = colors.mauve, bg = 'NONE', bold = true }, -- TODO: something
       MiniHipatternsNote = { fg = colors.sky, bg = 'NONE', bold = true }, -- NOTE
       MiniHipatternsWip = { fg = colors.mauve, bold = true }, -- WIP
       MiniHipatternsFix = { fg = colors.red, bold = true }, -- FIX
